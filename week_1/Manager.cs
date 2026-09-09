8c12207e30f0d001b2dc442aec092a8b9307ebed
Huba Trifusz <huba.trifusz@gmail.com>
2026-09-09 17:50:51 +0200

    feat: add manager class

---
+++ week_1/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_1
+{
+    public class Manager
+    {
+        private List<Todo> todos = new();
+
+        public void AddTodo(Todo todo)
+        {
+            todos.Add(todo);
+            Console.WriteLine($"{todo.Title} hozzáadva!");
+        }
+
+        public void ListTodos()
+        {
+            foreach (var t in todos)
+            {
+                Console.WriteLine(t.ToString());
+            }
+        }
+    }
+}
