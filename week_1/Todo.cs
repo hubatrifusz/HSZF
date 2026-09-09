94162512d57f42b2b878f82d106b4bb2e6c7722f
Huba Trifusz <huba.trifusz@gmail.com>
2026-09-09 17:36:10 +0200

    feat: create todo class

---
+++ week_1/Todo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week_1
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+
+        public Todo(int id, string title)
+        {
+            Id = id;
+            Title = title;
+            IsCompleted = false;
+        }
+
+        public override string ToString() {
+            return $"({(IsCompleted ? 'x' : ' ')} {Id} {Title})";
+        }
+    }
+}
